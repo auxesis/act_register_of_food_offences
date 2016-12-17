ec852584824437c80d7b69c9b12ad142376c79b5
Lindsay Holmwood <lindsay@holmwood.id.au>
2016-12-17 20:34:30 +1100

    First pass at the scraper. Builds correct number of businesses, but prosecutions all lumped together

--- scraper.rb
+++ scraper.rb
@@ -1,25 +1,260 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+require 'scraperwiki'
+require 'geokit'
+require 'pdf-reader'
+require 'pry'
+require 'nokogiri'
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find somehing on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+# Set an API key if provided
+Geokit::Geocoders::GoogleGeocoder.api_key = ENV['MORPH_GOOGLE_API_KEY'] if ENV['MORPH_GOOGLE_API_KEY']
 
-# You don't have to do things with the Mechanize or ScraperWiki libraries.
-# You can use whatever gems you want: https://morph.io/documentation/ruby
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+class String
+  def to_md5
+    Digest::MD5.new.hexdigest(self)
+  end
+
+  def scrub!
+    self.gsub!(/[[:space:]]/, ' ') # convert all utf whitespace to simple space
+    self.strip!
+  end
+end
+
+def normalise_date(value)
+  case
+  when value.class == DateTime
+    return value.to_date
+  when value.class == String
+    return Date.parse(value)
+  when value.class == Date
+    return value
+  else
+    puts "[debug] Unhandled date: #{value.inspect}"
+    raise
+  end
+end
+
+def generate_id(details)
+  return details.values.map(&:to_s).join(' ').to_md5
+end
+
+def geocode(prosecution)
+  @addresses ||= {}
+
+  address = prosecution['address']
+  address = [
+    prosecution['address'],
+    prosecution['county'],
+    prosecution['postcode'],
+  ].join(', ')
+
+  if @addresses[address]
+    puts "Geocoding [cache hit] #{address}"
+    location = @addresses[address]
+  else
+    puts "Geocoding #{address}"
+    a = Geokit::Geocoders::GoogleGeocoder.geocode(address)
+
+    if !a.lat && !a.lng
+      a = Geokit::Geocoders::GoogleGeocoder.geocode(prosecution['postcode'])
+    end
+
+    location = {
+      'lat' => a.lat,
+      'lng' => a.lng,
+    }
+
+    @addresses[address] = location
+  end
+
+  prosecution.merge!(location)
+end
+
+def existing_record_ids
+  return @cached if @cached
+  @cached = ScraperWiki.select('link from data').map {|r| r['link']}
+rescue SqliteMagic::NoSuchTable
+  []
+end
+
+def base
+  'http://www.health.act.gov.au'
+end
+
+def path
+  '/sites/default/files//Register%20of%20Food%20Offences_5.pdf'
+end
+
+def url
+  base + path
+end
+
+def last_known_link_text
+  'Register of Food Offences (updated 18 November 2016)'
+end
+
+# ACT health don't publish the PDF of the register of offences at a consistent
+# URL – it changes every time they publish a new one one. This method checks if
+# the PDF being linked to is different to the last known PDF.
+#
+# Looking at past copies of this page on the Wayback Machine, we know that:
+#
+# - The link text changes
+# - The link URL changes
+#
+# These are the URLs that have been used previously:
+#
+# - http://www.health.act.gov.au/sites/default/files/Register%20of%20Food%20Offences%20%28Last%20updated%2016%20March%202015%29.pdf
+# - http://health.act.gov.au/sites/default/files//Register%20of%20Food%20Offences%20%28updated%2017%20Sep%202015%29.pdf
+# - http://health.act.gov.au/sites/default/files//Register%20of%20Food%20Offences.pdf
+# - http://www.health.act.gov.au/sites/default/files//Register%20of%20Food%20Offences.pdf
+# - http://www.health.act.gov.au/sites/default/files//Register%20of%20Food%20Offences.pdf
+# - http://www.health.act.gov.au/sites/default/files//Register%20of%20Food%20Offences_0.pdf
+#
+# Note that some of them are duplicate, so there's no guarantee that if the
+# resource URL has not changed, the resource is not the same.
+#
+# It will exit if:
+#
+#  - The register link is not on the page. This could happen because the website
+#    is broken, the URL has changed, or the text they use in the link to the PDF
+#    has changed.
+#  - The URL to the PDF doesn't match the last known URL.
+#  - The link text doesn't match the last known link text.
+#
+# This is all a bit shit, because I don't want to have to update the scraper
+# every time the URL changes. ¯\_(ツ)_/¯
+#
+def abort_if_updated?
+  index = base + '/public-information/businesses/food-safety-regulation/register-food-offences'
+  page = open(index)
+  doc = Nokogiri::HTML(page)
+  register_link = doc.search('a').find {|a| a.text =~ /register.*updated/i }
+  register_link_text = register_link.text.strip
+
+  case
+  when !register_link
+    puts '[fatal] Could not find register link. Page source:'
+    puts "\n" + doc + "\n"
+    puts '[fatal] Exiting!'
+    exit 1
+  when register_link_text != last_known_link_text
+    puts '[fatal] Link text has changed!'
+    puts '[fatal] Expected: ' + last_known_link_text
+    puts '[fatal] Actual:   ' + register_link_text
+    puts '[fatal] Exiting!'
+    exit 2
+  when register_link['href'] != path
+    puts '[fatal] New register published at ' + register_link['href']
+    puts '[fatal] Exiting!'
+    exit 3
+  end
+end
+
+def extract_header(page)
+  page.find {|l| l =~ /^Prosecution Details/}
+end
+
+# Work out column names, and where they start and stop on each page
+def determine_columns(page)
+  columns = {}
+
+  header = extract_header(page)
+  names = header.split(/\s{2,}/)
+
+  names.each_cons(2) do |(first,last)|
+    start = header.index(first)
+    stop  = header.index(last) - 1
+    columns[first] = (start..stop)
+  end
+
+  start = header.index(names.last)
+  stop  = -1
+  columns[names.last] = (start..stop)
+
+  columns
+end
+
+# Strip header and footer from page, to return just the prosecutions
+def get_raw_records(page)
+  header = extract_header(page)
+  start = page.index(header) + 1
+  stop = -2 # last line is always a footer
+
+  page[start..stop]
+end
+
+def end_of_record?(column, value)
+  column == 'Offence Proven' && value =~ /^Total \(\d+\) Charge/
+end
+
+def add_to_record(column, value)
+  # Collection of all records
+  @records ||= []
+
+  # The record currently being processed
+  @record ||= {}
+
+  @record[column] ||= ''
+  @record[column] += ' ' if @record[column][-1] !~ /^\s$/ # add whitespace if none, so records don't smoosh together
+  @record[column] += value
+
+  # Work out if a new record needs to be created
+  if end_of_record?(column,value)
+    @records << @record
+    @record = nil
+  end
+end
+
+def build_records(raw_records, columns)
+  raw_records.each do |line|
+    columns.each do |column, range|
+      string = line[range]
+      if string && string.strip!
+        add_to_record(column, string)
+      end
+    end
+  end
+end
+
+# Iterate through all pages to build up records
+def extract_records_from_pages(pages)
+  pages.each do |page|
+    columns     = determine_columns(page)
+    raw_records = get_raw_records(page)
+    build_records(raw_records, columns)
+  end
+
+  binding.pry
+
+  @records
+end
+
+# Strips out empty lines
+def clean_pages(pages)
+  pages.map { |page| page.text.split("\n").reject {|l| l=~ /^\s*$/} }
+end
+
+def fetch_and_build_prosecutions
+  abort_if_updated?
+
+  io = open(url)
+  reader = PDF::Reader.new(io)
+
+  pages = clean_pages(reader.pages)
+  extract_records_from_pages(pages)
+end
+
+def main
+  prosecutions = fetch_and_build_prosecutions
+
+  puts "### Found #{prosecutions.size} notices"
+  new_prosecutions = prosecutions.select {|r| !existing_record_ids.include?(r['link'])}
+  puts "### There are #{new_prosecutions.size} new prosecutions"
+  new_prosecutions.map! {|p| geocode(p) }
+
+  # Serialise
+  ScraperWiki.save_sqlite(['link'], new_prosecutions)
+
+  puts "Done"
+end
+
+main()
